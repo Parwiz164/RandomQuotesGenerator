28497553d3503d81ad914da1ef89553a5db40a05
Parwiz164 <p.ahmadi90@outlook.com>
2017-12-19 00:01:27 +0100

    Modified code

--- js/script.js
+++ js/script.js
@@ -2,3 +2,39 @@
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
+
+let quotesArray = [
+    {
+        quote: "Don't cry because it's over, smile because it happened.",
+        source: "Dr. Seuss",
+        citation: "",
+        year:2017 
+    },
+    {
+        quote: "Be yourself; everyone else is already taken.",
+        source: "Oscar Wilde",
+        citation: "",
+        year:2016 
+    },
+    {
+        quote: "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.",
+        source: "Albert Einstein",
+        citation: "",
+        year:2015 
+    },
+];
+
+function getRandomQuote(){
+    let randomQuote = quotesArray[Math.floor(Math.random()*quotesArray.length)];
+    return randomQuote;
+}
+
+function printQuote(){
+    let print = getRandomQuote();
+
+    let quote = "<p> " + print.quote + "</p><p>" + print.source + "<span>" + print.citation + "</span><span>" + print.year + "</span></p>";
+
+    document.getElementById('quote-box').innerHTML = quote;
+
+
+}
